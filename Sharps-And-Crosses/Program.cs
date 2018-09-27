607bae8dd6d4cfbfa610a906c92207a5a93cb149
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-09-27 16:45:31 -0400

    Practicing condiditonals, has a single problem to solve and can read input from the console.

--- Sharps-And-Crosses/Program.cs
+++ Sharps-And-Crosses/Program.cs
@@ -10,7 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You Lose!");
+            Console.WriteLine(@"1|2|3");
+            Console.WriteLine(@"-----");
+            Console.WriteLine(@"4|x|o");
+            Console.WriteLine(@"-----");
+            Console.WriteLine(@"x|8|o");
+
+            Console.WriteLine("Your turn x, select a spot to mark");
+
+            string play = Console.ReadLine();
+
+            if (play == "3")
+            Console.WriteLine("You Win!");
+            else
+                Console.WriteLine("You Lose!");
         }
     }
 }
